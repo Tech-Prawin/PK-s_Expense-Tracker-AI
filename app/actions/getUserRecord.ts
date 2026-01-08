453bfd27d3609c0d6d23f0d55255218436a0d61d
Tech-Prawin <prawinkumarmcseb@gmail.com>
2026-01-08 21:55:15 +0530

    guserrecord change

--- app/actions/getUserRecord.ts
+++ app/actions/getUserRecord.ts
@@ -1,4 +1,5 @@
 'use server';
+
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs/server';
 
@@ -18,16 +19,18 @@ async function getUserRecord(): Promise<{
       where: { userId },
     });
 
-    const record = records.reduce((sum, record) => sum + record.amount, 0);
+    const record = records.reduce(
+      (sum: number, record: { amount: number }) => sum + record.amount,
+      0
+    );
 
-    // Count the number of days with valid sleep records
     const daysWithRecords = records.filter(
-      (record) => record.amount > 0
+      (record: { amount: number }) => record.amount > 0
     ).length;
 
     return { record, daysWithRecords };
   } catch (error) {
-    console.error('Error fetching user record:', error); // Log the error
+    console.error('Error fetching user record:', error);
     return { error: 'Database error' };
   }
 }
