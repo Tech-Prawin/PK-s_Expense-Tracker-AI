cf860db43fa1530859cdd3487a8449c55bbb7fa4
Tech-Prawin <prawinkumarmcseb@gmail.com>
2026-01-08 22:15:46 +0530

    primsa error change

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
 });
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
 export const db =
-  globalThis.prisma ??
-  new PrismaClient({ adapter });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = db;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db;
 }
